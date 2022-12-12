32b1865c4516c74862e0af352d3372af0d0a08d3
JMcCormick <62381265+McCormickJoshua@users.noreply.github.com>
2022-12-12 17:53:56 -0500

    Update chad_sauce.py
    change csv load; pull ticker column

--- chad_sauce.py
+++ chad_sauce.py
@@ -9,14 +9,6 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def add_good_stocks(csv_filename):
-    symbols = []
-    print('Scanning stocks...')
-    with open(csv_filename, newline='') as f:
-        for row in csv.reader(f):
-            symbols.append(row[0])
-    return symbols[1:]
-
 def add_stos(df):
     # Initialize Stochastic Indicator
     indicator_slow6 = StochasticOscillator(high=df["High"], low=df["Low"], close=df["Close"], window=6, smooth_window=1)
@@ -101,9 +93,10 @@ def send_email(stocks_to_see, recipients):
 
 # This is where the program starts
 def main():
-    stocks_to_see = []
+    stonks = pd.read_csv('good_stocks.csv')
+    stocks_to_see = stonks.Ticker.tolist()
     # Iterate through each stock from the stock screener, add indicators, and find good setups
-    for index, symbol in enumerate(add_good_stocks('good_stocks.csv')):
+    for index, symbol in enumerate(stocks_to_see):
         stock = yf.Ticker(symbol)
         try:
             df = stock.history(period="1y")
